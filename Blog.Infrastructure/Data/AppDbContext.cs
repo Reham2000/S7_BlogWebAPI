cb02bf3e2262268dd48366a9fb8725ada31a7d88
Reham2000 <rehamabado@gmail.com>
2025-09-08 17:47:47 +0300

    Session 1

---
+++ Blog.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Infrastructure.Data
+{
+    // class to control database
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
+    
+        // Set Database Tables
+        public DbSet<Post> Posts { get; set; }
+    
+    }
+}
